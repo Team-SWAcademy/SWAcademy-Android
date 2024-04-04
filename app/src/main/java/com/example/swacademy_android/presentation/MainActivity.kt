395d76b784f5f2789eac3708b70eecbca0e89df4
Seohyeon Shin <ss99x200211@gmail.com>
2024-04-04 20:57:03 +0900

    [Feat/#5] MainActivity Bottom nav 추가, Fragment 추가

--- app/src/main/java/com/example/swacademy_android/presentation/MainActivity.kt
+++ app/src/main/java/com/example/swacademy_android/presentation/MainActivity.kt
@@ -1,14 +1,56 @@
 package com.example.swacademy_android.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.swacademy_android.R
+import com.example.swacademy_android.databinding.ActivityMainBinding
+import com.example.swacademy_android.util.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        context = this
+        initMainView()
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+
+        with(binding){
+            bottomNavigationMain.background = null
+            bottomNavigationMain.menu.getItem(1).isEnabled = false
+            fabCamera.setOnClickListener {
+                startActivity(Intent(this@MainActivity,CameraActivity::class.java))
+            }
+        }
+
+        binding.bottomNavigationMain.run {
+            setOnItemSelectedListener {
+                changeFragment(
+                when(it.itemId) {
+                    R.id.menu_home -> HomeFragment()
+                    else -> MypageFragment()
+                })
+                true
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
+    }
+    private fun initMainView() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        if (currentFragment == null) {
+            changeFragment(HomeFragment())
+        }
     }
 }
