a96a7cdddc14671e319ee90e0e2fdeef23358a62
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-05-07 12:24:46 +0900

    [Feat/#12] 반납 버튼 누를 시 QR 카메라로 이동 기능 구현

--- app/src/main/java/com/example/swacademy_android/presentation/MultiuseDetailActivity.kt
+++ app/src/main/java/com/example/swacademy_android/presentation/MultiuseDetailActivity.kt
@@ -3,7 +3,11 @@ package com.example.swacademy_android.presentation
 import com.example.swacademy_android.databinding.FragmentMultiuseDetailBinding
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 class MultiuseDetailActivity : AppCompatActivity() {
 
     lateinit var binding: FragmentMultiuseDetailBinding
@@ -13,7 +17,22 @@ class MultiuseDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.clMultiuseDetailReturnBtn.setOnClickListener {
-            startActivity(Intent(this, CameraActivity::class.java))
+            val options = ScanOptions()
+            options.apply{
+                setOrientationLocked(false)
+                setPrompt("화면에 QR코드를 스캔해주세요")
+            }
+            qrCodeLauncher.launch(options)
+        }
+    }
+
+    private val qrCodeLauncher = registerForActivityResult(ScanContract()) {
+            result : ScanIntentResult ->
+        if (result.contents == null) {
+            Toast.makeText (this, "Cancelled" , Toast.LENGTH_LONG).show()
+        }
+        else {
+            Toast.makeText (this, "Scanned:" + result.contents, Toast.LENGTH_LONG).show()
         }
     }
 
