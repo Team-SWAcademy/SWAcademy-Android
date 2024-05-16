e0bee6f753453c4ab14c0936c22ab45bbbdecae6
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-05-16 15:05:10 +0900

    [Feat/#24] 지도 기능 구현

--- app/src/main/java/com/example/swacademy_android/App.kt
+++ app/src/main/java/com/example/swacademy_android/App.kt
@@ -1,7 +1,10 @@
 package com.example.swacademy_android
 
 import android.app.Application
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.util.Utility
+import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,6 +12,8 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         stopDarkMode()
+
+        KakaoMapSdk.init(this, "06fb8cd47af6f890938556cf824bdae3")
     }
 
     private fun stopDarkMode(){
