eaa6725f74b5e279e2e86f4e7575b9a7bef920bc
Seohyeon Shin <ss99x200211@gmail.com>
2024-05-28 13:47:37 +0900

    [Feat/#32] 닉네임 수정 API 연결

--- app/src/main/java/com/example/swacademy_android/data/service/MypageService.kt
+++ app/src/main/java/com/example/swacademy_android/data/service/MypageService.kt
@@ -1,9 +1,16 @@
 package com.example.swacademy_android.data.service
 
+import com.example.swacademy_android.data.model.request.MypageEditNameRequestDto
+import com.example.swacademy_android.data.model.response.MypageEditNameResponseDto
 import com.example.swacademy_android.data.model.response.MypageResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 
 interface MypageService {
     @GET("/api/v1/users/my-page")
     suspend fun getMypageData() : MypageResponseDto
+
+    @PATCH ("/api/v1/users/my-page/edit")
+    suspend fun patchMypageUserName(@Body editNickName: String) :MypageEditNameResponseDto
 }
