6967c820017f95c5cae9056fca6e498fd9e85dea
Seohyeon Shin <ss99x200211@gmail.com>
2024-05-28 13:48:14 +0900

    [Feat/#32] 닉네임 수정 API fragment 연결

--- app/src/main/java/com/example/swacademy_android/presentation/mypage/MypageViewModel.kt
+++ app/src/main/java/com/example/swacademy_android/presentation/mypage/MypageViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.swacademy_android.data.model.request.MypageEditNameRequestDto
 import com.example.swacademy_android.data.model.response.HomeResponseDto
 import com.example.swacademy_android.data.model.response.MypageResponseDto
 import com.example.swacademy_android.domain.repository.MypageRepository
@@ -22,6 +23,8 @@ class MypageViewModel @Inject constructor(private val mypageRepository: MypageRe
 
     private var _mypageResponse = MutableLiveData<MypageResponseDto.Result>()
     val mypageResponse: LiveData<MypageResponseDto.Result> = _mypageResponse
+
+    var _editNickName = MutableLiveData<String>()
     private fun getMypageData() {
         viewModelScope.launch {
             mypageRepository.getMypageData().onSuccess {
@@ -29,9 +32,27 @@ class MypageViewModel @Inject constructor(private val mypageRepository: MypageRe
             }.onFailure { error ->
                 if (error is HttpException) {
                     val errorBody = error.response()?.errorBody()?.string()
-                    Log.e("hyeon", "next question http 연결 실패 $errorBody")
+                    Log.e("hyeon", " http 연결 실패 $errorBody")
                 }
-                Log.e("hyeon", "next question 실패 ${error.message}")
+                Log.e("hyeon", "실패 ${error.message}")
+            }
+        }
+    }
+
+    fun patchMypageUserName(editNickName: String) {
+        Log.e("hyeon","viewModel에서 받은 name " + editNickName)
+        viewModelScope.launch {
+            mypageRepository.patchMypageUserName(editNickName).onSuccess {
+                Log.e("hyeon","viewModel에서 받은 name " + editNickName)
+                Log.e("hyeon","viewModel" + it.result.editNickname.toString())
+                Log.e("hyeon","mypage viewModel response ${it}")
+                _editNickName.value = if( it.result.editNickname != null )  it.result.editNickname else ""
+            }.onFailure { error ->
+                if (error is HttpException) {
+                    val errorBody = error.response()?.errorBody()?.string()
+                    Log.e("hyeon", "http 연결 실패 $errorBody")
+                }
+                Log.e("hyeon", "실패 ${error.message}")
             }
         }
     }
