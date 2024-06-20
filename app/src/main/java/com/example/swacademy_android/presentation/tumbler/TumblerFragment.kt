f752e730f618b16f47a03f5b9869c65a0b21a934
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-20 16:57:47 +0900

    [Design/#44] 영수증 화면 fragment 구현

---
+++ app/src/main/java/com/example/swacademy_android/presentation/tumbler/TumblerFragment.kt
@@ -0,0 +1,29 @@
+package com.example.swacademy_android.presentation.tumbler
+
+import android.os.Bundle
+import android.view.View
+import com.example.swacademy_android.R
+import com.example.swacademy_android.databinding.FragmentTumblerBinding
+import com.example.swacademy_android.util.BindingFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TumblerFragment: BindingFragment<FragmentTumblerBinding>(R.layout.fragment_tumbler) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClickVerifyCamera()
+        onClickVerifyPhoto()
+    }
+
+    private fun onClickVerifyCamera() {
+        binding.clVerifyCamera.setOnClickListener {
+            // 카메라로 인증하기 버튼 누를시
+        }
+    }
+
+    private fun onClickVerifyPhoto() {
+        binding.clVerifyPhoto.setOnClickListener {
+            // 앨범 사진으로 인증하기 버튼 누를시
+        }
+    }
+}
